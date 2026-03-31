67cd6a60429f7dfefee8225a1a8fea306f36e20a
Дима Ш. <dima.o.sh@proton.me>
2026-03-31 20:54:02 +0100

    Remove random two additional levels
    Co-authored-by: rolyPolyVole <yezhanting@gmail.com>

--- src/main/java/io/gitlab/shdima/anvils/mixins/AnvilScreenHandlerMixin.java
+++ src/main/java/io/gitlab/shdima/anvils/mixins/AnvilScreenHandlerMixin.java
@@ -1,7 +1,5 @@
 package io.gitlab.shdima.anvils.mixins;
 
-import net.minecraft.component.ComponentType;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
@@ -12,9 +10,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -41,6 +37,7 @@ abstract public class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 	private void canTakeOutput(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> cir) {
 		if (anvils$shouldBeFree()) cir.setReturnValue(true);
 	}
+
 	@Inject(method = "onTakeOutput", at = @At("HEAD"))
 	private void onTakeOutput(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
 		if (anvils$shouldBeFree()) levelCost.set(0);
@@ -51,26 +48,23 @@ abstract public class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 		if (anvils$shouldBeFree()) levelCost.set(0);
 	}
 
-	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
-	private <T> T getOrDefault(ItemStack instance, ComponentType<? extends T> componentType, T o) {
-		if (componentType == DataComponentTypes.REPAIR_COST && anvils$shouldBeFree()) return o;
-		return instance.getOrDefault(componentType, o);
+	@ModifyVariable(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/item/ItemStack;setDamage(I)V",
+					shift = At.Shift.AFTER,
+					ordinal = 1
+			),
+			index = 2
+	)
+	private int removeUselessTwoLevels(int value) {
+		return value - 2;
 	}
 
 	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I"))
 	private int getItemLevelCost(Property instance) {
 		if (anvils$shouldBeFree()) return 0;
 		return instance.get();
-	}
-
-	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;set(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
-	private <T> @Nullable T setRepairCost(ItemStack instance, ComponentType<T> type, T value) {
-		if (type == DataComponentTypes.REPAIR_COST && anvils$shouldBeFree()) return null;
-		return instance.set(type, value);
-	}
-
-	@Inject(method = "getLevelCost", at = @At("HEAD"), cancellable = true)
-	private void getLevelCost(CallbackInfoReturnable<Integer> cir) {
-		if (anvils$shouldBeFree()) cir.setReturnValue(0);
 	}
 }
