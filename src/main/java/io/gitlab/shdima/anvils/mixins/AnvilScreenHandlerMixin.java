d364423f4c168591840ecc5a15c07a8b72942016
rolyPolyVole <yezhanting@gmail.com>
2026-03-31 23:21:40 +0100

    Fully implement mod functionality
    Co-authored-by: Дима Ш. <dima.o.sh@proton.me>

--- src/main/java/io/gitlab/shdima/anvils/mixins/AnvilScreenHandlerMixin.java
+++ src/main/java/io/gitlab/shdima/anvils/mixins/AnvilScreenHandlerMixin.java
@@ -1,5 +1,9 @@
 package io.gitlab.shdima.anvils.mixins;
 
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
@@ -21,16 +25,30 @@ abstract public class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 	@Final
 	private Property levelCost;
 
+	@Unique
+	private boolean anvils$hasRepaired;
+	@Unique
+	private boolean anvils$hasRenamed;
+	@Unique
+	private int anvils$originalAnvilUses;
+
 	public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context, ForgingSlotsManager forgingSlotsManager) {
 		super(type, syncId, playerInventory, context, forgingSlotsManager);
 	}
 
 	@Unique
 	private boolean anvils$shouldBeFree() {
-		ItemStack firstStack = this.input.getStack(0);
-		ItemStack resultStack = this.output.getStack(2);
+		ItemStack firstStack = this.input.getStack(0).copy();
+		ItemStack resultStack = this.output.getStack(0).copy();
 
 		return firstStack.getEnchantments().equals(resultStack.getEnchantments());
+	}
+
+	@Unique
+	private boolean anvils$shouldBeFree(ItemStack result) {
+		ItemStack firstStack = this.input.getStack(0).copy();
+
+		return firstStack.getEnchantments().equals(result.copy().getEnchantments());
 	}
 
 	@Inject(method = "canTakeOutput", at = @At("HEAD"), cancellable = true)
@@ -45,21 +63,99 @@ abstract public class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
 	@Inject(method = "updateResult", at = @At("TAIL"))
 	private void updateResult(CallbackInfo ci) {
+		this.anvils$hasRepaired = false;
+		this.anvils$hasRenamed = false;
+		this.anvils$originalAnvilUses = 0;
+
 		if (anvils$shouldBeFree()) levelCost.set(0);
 	}
 
-	@ModifyVariable(
+	@Inject(
 			method = "updateResult",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/item/ItemStack;setDamage(I)V",
-					shift = At.Shift.AFTER,
 					ordinal = 1
-			),
-			index = 2
+			)
 	)
-	private int removeUselessTwoLevels(int value) {
-		return value - 2;
+	private void checkHasRepaired(CallbackInfo ci) {
+		this.anvils$hasRepaired = true;
+	}
+
+	@Inject(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/item/ItemStack;set(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;",
+					ordinal = 0
+			)
+	)
+	private void checkHasRenamed(CallbackInfo ci) {
+		this.anvils$hasRenamed = true;
+	}
+
+	@Inject(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/item/ItemStack;remove(Lnet/minecraft/component/ComponentType;)Ljava/lang/Object;"
+			)
+	)
+	private void checkHasRenamedAgain(CallbackInfo ci) {
+		this.anvils$hasRenamed = true;
+	}
+
+	@Redirect(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/screen/Property;set(I)V",
+					ordinal = 5
+			)
+	)
+	private void removeRepairAndRenameCosts(Property instance, int value) {
+		int newValue = value;
+
+		if (anvils$hasRenamed) {
+			newValue--;
+		}
+
+		if (anvils$hasRepaired) {
+			newValue -= 2;
+		}
+
+		instance.set(newValue);
+	}
+
+	@Redirect(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/item/ItemStack;set(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;",
+					ordinal = 1
+			)
+	)
+	private <T> T saveAnvilUses(ItemStack instance, ComponentType<T> type, @Nullable T value) {
+		this.anvils$originalAnvilUses = (int) instance.get(type);
+
+		instance.set(type, value);
+
+		return null;
+	}
+
+	@Redirect(
+			method = "updateResult",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/enchantment/EnchantmentHelper;set(Lnet/minecraft/item/ItemStack;Lnet/minecraft/component/type/ItemEnchantmentsComponent;)V"
+			)
+	)
+	private void removeAnvilUseIncrease(ItemStack stack, ItemEnchantmentsComponent enchantments) {
+		EnchantmentHelper.set(stack, enchantments);
+
+		if (anvils$shouldBeFree(stack)) {
+			stack.set(DataComponentTypes.REPAIR_COST, anvils$originalAnvilUses);
+		}
 	}
 
 	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I"))
